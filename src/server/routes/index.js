580a8f6cd56943efcee5249ed57d222a98efb312
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-07 20:09:55 -0700

    Added notes about potential routes necessary

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,4 +5,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/*
+  1. index  '/'                                           GET
+  2. products  '/products'                                GET
+  3. checkout  '/checkout'                                GET/POST
+  4. log in  '/sign_in'                                   GET/POST
+  5. sign-up  '/sign_up'                                  GET/POST
+  6. user-account '/my_account'                           GET
+  7. admin dashboard '/admin'                             GET
+  8. add product '/products/add'                          GET/POST
+  9. edit product '/products/:id/edit'                    GET/POST
+  10. delete product '/products/:id/delete'               POST
+  11. log out '/logout'                                   GET
+*/
+
 module.exports = router;
