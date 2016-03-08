ed09a16b398082daa7a1d83381a399c9a27353ed
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-03-07 20:46:51 -0700

    Added skeleton routes to index.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var express   = require('express');
+var router    = express.Router();
+var queries   = require('../queries.js');
 
 /*
   1. index  '/'                                           GET
@@ -18,5 +15,92 @@ router.get('/', function(req, res, next) {
   10. delete product '/products/:id/delete'               POST
   11. log out '/logout'                                   GET
 */
+
+router.get('/', function(req, res, next) {
+  // render the index page with a few selected products from the DB.
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/products', function(req, res, next) {
+  // get all products to render the product page.
+  res.render('products', {title: 'Products'});
+});
+
+router.post('/products/:id/add_cart', function(req, res, next) {
+  // not sure if necessary, adds the specified ID to the shopping cart.
+  // possibly using session storage before inserting to the database?
+  res.redirect('/products');
+});
+
+router.get('/checkout', function(req, res, next) {
+  // render the checkout form.
+  res.render('checkout', {title: 'Checkout'});
+});
+
+router.post('/checkout', function(req, res, next) {
+  // submit the checkout form, query the stripe API, and redirect to home after success.
+  res.redirect('/');
+});
+
+router.get('/sign_in', function(req, res, next) {
+  // render the sign in page.
+  res.render('login', {title: 'Sign In'});
+});
+
+router.post('/sign_in', function(req, res, next) {
+  // sign the user in if their email exists in the DB and the password matches.
+  res.redirect('/');
+});
+
+router.get('/sign_up', function(req, res, next) {
+  // render the sign up page.
+  res.render('signup', {title: 'Sign Up'});
+});
+
+router.post('/sign_up', function(req, res, next) {
+  // add the user to the DB.
+  res.redirect('/');
+});
+
+router.get('/my_account', function(req, res, next) {
+  // render the my_account page to show users past orders.
+  res.render('user_account', {title: 'Your Account'});
+});
+
+router.get('/admin', function(req, res, next) {
+  // render the admin page in order to see all orders.
+  res.render('admin', {title: 'Admin Dashboard'});
+});
+
+router.get('/products/add', function(req, res, next) {
+  // render the add_edit page to add a new product. **ADMIN ONLY**
+  res.render('add_edit', {title: 'Add New Product'});
+});
+
+router.post('/products/add', function(req, res, next) {
+  // submit the add product page to add a new product to the DB. **ADMIN ONLY**
+  res.redirect('/admin');
+});
+
+router.get('/products/:id/edit', function(req, res, next) {
+  // render the add_edit page to edit a product. **ADMIN ONLY**
+  res.render('add_edit', {title: 'Edit Product'});
+});
+
+router.post('/products/:id/edit', function(req, res, next) {
+  // submit the edit form to update the product in the DB. **ADMIN ONLY**
+  res.redirect('/admin');
+});
+
+router.post('/products/:id/delete', function(req, res, next) {
+  // delete a product from the database. **ADMIN ONLY**
+  res.redirect('/admin');
+});
+
+router.post('/logout', function(req, res, next) {
+  // log out the user and redirect to the index page.  Possibly with log out connect-flash message.
+  res.redirect('/');
+});
+
 
 module.exports = router;
